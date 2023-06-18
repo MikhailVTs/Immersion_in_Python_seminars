c402f29ccda948aedf0b2e944d2bef3d21d50691
Tsaplin Mikhail <caplin.m@inbox.ru>
2023-06-19 07:00:55 +1000

    Добавление в алгоритм решения нахождения произведения цифр 2-го числа

--- Seminar_1/task_1/InputNumber.py
+++ Seminar_1/task_1/InputNumber.py
@@ -1,2 +1,24 @@
+multiplication = 1
+
 while True:
-    
+    number = int(input("Введите число от 1 до 999 "))
+
+    if number >= 1 and number <= 999:
+
+        if len(str(number)) == 1:
+            print(pow(number, 2))
+
+        elif len(str(number)) == 2:
+            while number > 0:
+                digit = number % 10
+                multiplication *= digit
+                number //= 10
+            print(multiplication)
+
+        else:
+            pass
+
+    else:
+        result = "Не входит в диапозон"
+
+print(result)
