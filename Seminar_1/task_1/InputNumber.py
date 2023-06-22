26cbd32062c968d6fc249a4c13012f2ead7eb118
Tsaplin Mikhail <caplin.m@inbox.ru>
2023-06-22 19:39:18 +1000

    Добавление алгоритма, зеркальное отображение.

--- Seminar_1/task_1/InputNumber.py
+++ Seminar_1/task_1/InputNumber.py
@@ -1,24 +1,31 @@
 multiplication = 1
 
 while True:
-    number = int(input("Введите число от 1 до 999 "))
+    number = int(input("\nВведите число от 1 до 999: "))
 
     if number >= 1 and number <= 999:
 
         if len(str(number)) == 1:
-            print(pow(number, 2))
+            result = pow(number, 2)
 
         elif len(str(number)) == 2:
             while number > 0:
-                digit = number % 10
-                multiplication *= digit
+                num = number % 10
+                multiplication *= num
                 number //= 10
-            print(multiplication)
-
+            result = multiplication
+        
         else:
-            pass
+
+            reversal = 0
+            while number != 0:
+                num_rev = number % 10
+                reversal = reversal * 10 + num_rev
+                number //= 10
+            result = reversal
+            
 
     else:
         result = "Не входит в диапозон"
-
-print(result)
+    
+    print(result)
