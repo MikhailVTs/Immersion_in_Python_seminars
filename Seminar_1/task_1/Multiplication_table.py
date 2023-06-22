28bdbdd24d8ceb9cacdf6b2d3e819f7a261d93c6
Tsaplin Mikhail <caplin.m@inbox.ru>
2023-06-22 23:19:14 +1000

    Редактирование вывода результата

--- Seminar_1/task_1/Multiplication_table.py
+++ Seminar_1/task_1/Multiplication_table.py
@@ -1,13 +1,13 @@
 print(f'\n{"-" * 60}')
 
-for i in range (2, 11):
+for i in range(2, 11):
     for j in range(2, 6):
         print(f'{j} * {i} = {i * j}', end='\t')
     print()
 
 print()
 
-for i in range (2, 11):
+for i in range(2, 11):
     for j in range(6, 10):
         print(f'{j} * {i} = {i * j}', end='\t')
     print()
