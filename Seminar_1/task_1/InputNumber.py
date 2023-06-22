088b0daf8f81cfd86e51f7c609f20180b1799920
Tsaplin Mikhail <caplin.m@inbox.ru>
2023-06-22 22:24:57 +1000

    Редактирование вывода результата

--- Seminar_1/task_1/InputNumber.py
+++ Seminar_1/task_1/InputNumber.py
@@ -1,31 +1,33 @@
-multiplication = 1
-
 while True:
+    multiplication = 1
+    x_number = 1
     number = int(input("\nВведите число от 1 до 999: "))
 
     if number >= 1 and number <= 999:
 
         if len(str(number)) == 1:
-            result = pow(number, 2)
+
+            result = f'\nКвадрат числа {number} = {pow(number, 2)}'
 
         elif len(str(number)) == 2:
+            x_number = number
             while number > 0:
+
                 num = number % 10
                 multiplication *= num
                 number //= 10
-            result = multiplication
-        
-        else:
+            result = f'\nПроизведение числа {x_number} = {multiplication}'
 
+        else:
             reversal = 0
+            x_number = number
             while number != 0:
                 num_rev = number % 10
                 reversal = reversal * 10 + num_rev
                 number //= 10
-            result = reversal
-            
+            result = f'\nЗеркальное отображение числа {x_number} = {reversal}'
 
     else:
-        result = "Не входит в диапозон"
-    
+        result = "\nНе входит в диапозон"
+
     print(result)
