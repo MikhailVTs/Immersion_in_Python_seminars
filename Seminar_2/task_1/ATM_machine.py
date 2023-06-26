e9d3f410c45555e066158fada76a0abfa351a912
Tsaplin Mikhail <caplin.m@inbox.ru>
2023-06-27 01:04:57 +1000

    Решение алгоритма, программа Банкомат

--- Seminar_2/task_1/ATM_machine.py
+++ Seminar_2/task_1/ATM_machine.py
@@ -15,32 +15,33 @@ counter = 0
 
 while True:
 
-
-
     print("Выберите действие согласно цифре")
-    menu = int(input(f'Пополнить счёт - 1, снять со счёта деньги - 2, выйти - 0: '))
+    menu = int(
+        input(f'Пополнить счёт - 1, снять со счёта деньги - 2, выйти - 0: '))
     if menu == 0:
         print(f'Заберите карту, на Вашем счету {in_your_bank_account} руб.')
         break
     if in_your_bank_account > wealth_tax_limit:
         percent = in_your_bank_account * interest_on_wealth
         in_your_bank_account -= percent
-        print(f'Будет произведен вычет на богатство {interest_on_wealth}% в сумме {percent} руб. На Вашем банковском счёте {in_your_bank_account} руб.')
+        print(
+            f'Будет произведен вычет на богатство {interest_on_wealth}% в сумме {percent} руб. На Вашем банковском счёте {in_your_bank_account} руб.')
     if menu in (1, 2):
         amount = 1
         while (amount % multiplicity_number) != 0:
-            amount = decimal.Decimal(input(f'Введите сумму кратную {multiplicity_number} руб. '))
+            amount = decimal.Decimal(
+                input(f'Введите сумму кратную {multiplicity_number} руб. '))
     if menu == 1:
         counter += 1
-        in_your_bank_account += amount     
-        result = f'Счёт пополнен на {amount} руб. на Вашем банковском счёте {in_your_bank_account} руб.'   
+        in_your_bank_account += amount
+        result = f'Счёт пополнен на {amount} руб. на Вашем банковском счёте {in_your_bank_account} руб.'
     elif menu == 2:
         percent = amount * commission
         percent = minimum_commission if percent < minimum_commission else maximum_commission if percent > maximum_commission else percent
-        
-        if in_your_bank_account >= amount + percent:            
-            counter += 1           
-            in_your_bank_account = in_your_bank_account - amount - percent            
+
+        if in_your_bank_account >= amount + percent:
+            counter += 1
+            in_your_bank_account = in_your_bank_account - amount - percent
             result = f'Произведено снятие с Вашего счёта {amount} руб. Процент за снятие составляет {percent}. на Вашем банковском счёте {in_your_bank_account} руб.'
         else:
             result = f'Недостаточно средств на Вашем счёте. Сумма с комиссией {amount + percent} руб. На Вашем счету {in_your_bank_account} руб.'
