c57b4703d8d7ce664d098e5904670cc40b5f289b
Tsaplin Mikhail <caplin.m@inbox.ru>
2023-06-27 01:20:47 +1000

    Решение алгоритма, которое принимает две строки вида “a/b” — дробь с числителем и знаменателем

--- Seminar_2/task_3/Number_fruction.py
+++ Seminar_2/task_3/Number_fruction.py
@@ -6,14 +6,18 @@ fraction_2_numerator = (input("Введите число в числитель �
 fraction_2_denominator = (input("Введите число в знаменатель второй дроби: "))
 
 
-result_fraction_plus = int(fraction_1_numerator) / int(fraction_1_denominator) + int(fraction_2_numerator) / int(fraction_2_denominator)
-result_fraction_multiplication = int(fraction_1_numerator) / int(fraction_1_denominator) * int(fraction_2_numerator) / int(fraction_2_denominator)
+result_fraction_plus = int(fraction_1_numerator) / int(fraction_1_denominator) + \
+    int(fraction_2_numerator) / int(fraction_2_denominator)
+result_fraction_multiplication = int(fraction_1_numerator) / int(
+    fraction_1_denominator) * int(fraction_2_numerator) / int(fraction_2_denominator)
 
 print(result_fraction_plus)
 print(result_fraction_multiplication)
 
-fraction_1 = fractions.Fraction(int(fraction_1_numerator), int(fraction_1_denominator))
-fraction_2 = fractions.Fraction(int(fraction_2_numerator), int(fraction_2_denominator))
+fraction_1 = fractions.Fraction(
+    int(fraction_1_numerator), int(fraction_1_denominator))
+fraction_2 = fractions.Fraction(
+    int(fraction_2_numerator), int(fraction_2_denominator))
 
 fraction_plus = fraction_1 + fraction_2
 fraction_multiplication = fraction_1 * fraction_2
