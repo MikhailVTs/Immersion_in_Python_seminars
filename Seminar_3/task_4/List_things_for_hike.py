4a28d3ba73b76c0e27918b38e5a6c466b8f8f308
Tsaplin Mikhail <caplin.m@inbox.ru>
2023-06-30 01:02:33 +1000

    Создание файла List_things_for_hike.py

---
+++ Seminar_3/task_4/List_things_for_hike.py
@@ -0,0 +1,23 @@
+import random
+from random import sample
+bags = {"Хлеб": 69, "Вода": 15, "Соль": 52, "Картофель": 24, "Котелок": 25, "Спички": 15, "Нож": 33}
+
+bag_weight = 0
+bag_capacity = 100
+
+list_things_in_bag = []
+
+
+for i in range(len(bags)):
+
+    while bag_weight < bag_capacity:
+
+            key, value = random.choice(list(bags.items()))
+            if key in list_things_in_bag:
+                continue
+            if bag_weight + value > bag_capacity:
+                break
+            bag_weight += value
+            list_things_in_bag += (str(key), str(value))
+    print(f"\nВещи, которые поместились в мешок - {list_things_in_bag}\nВес мешка составил -  {bag_weight}\n")
+
