3e2f91519ff32a8b7b9a3ec790e42274ccdd46c2
Tsaplin Mikhail <caplin.m@inbox.ru>
2023-07-03 23:30:33 +1000

    Алгоритм запуска Меню

--- Seminar_4/task_3/Menu.py
+++ Seminar_4/task_3/Menu.py
@@ -19,8 +19,6 @@ def menu():
     global amount
     global list_result
 
-
-
     multiplicity_number = 50
     commission = decimal.Decimal(15) / decimal.Decimal(1000)
     minimum_commission = decimal.Decimal(30)
@@ -30,10 +28,9 @@ def menu():
     interest_on_wealth = decimal.Decimal(10) / decimal.Decimal(100)
     wealth_tax_limit = decimal.Decimal(10_000_000)
     in_your_bank_account = decimal.Decimal(0)
-    
+
     list_result = []
     counter = 0
-
 
     while True:
 
@@ -42,7 +39,8 @@ def menu():
             input(f'Пополнить счёт - 1, снять со счёта деньги - 2, выйти - 0: '))
         if menu == 0:
 
-            print(f'Заберите карту, на Вашем счету {in_your_bank_account} руб.')
+            print(
+                f'Заберите карту, на Вашем счету {in_your_bank_account} руб.')
             break
         if in_your_bank_account > wealth_tax_limit:
             wealth_tax()
@@ -55,8 +53,7 @@ def menu():
             account_replenishment()
         elif menu == 2:
             withdrawal_account()
-            
-            
+
         else:
             result = f'Неверное действие для карты на которой {in_your_bank_account} руб.'
             if counter % percent_counter == 0:
