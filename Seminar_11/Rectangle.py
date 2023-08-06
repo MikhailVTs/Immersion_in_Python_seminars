0794a9c6eacb1db6e7313841e8b0a3856e56498d
Tsaplin Mikhail <caplin.m@inbox.ru>
2023-08-06 15:16:16 +1000

    Модернизация класса прямоугольник

--- Seminar_11/Rectangle.py
+++ Seminar_11/Rectangle.py
@@ -1,21 +1,20 @@
 class Rectangle:
 
-
     def __init__(self, side_a: int, side_b: int = None):
 
         self.side_a = side_a
         self.side_b = side_b if side_b is not None else side_a
 
     def perimeter(self):
- 
+
         return 2 * (self.side_a + self.side_b)
 
     def area(self):
-  
+
         return self.side_a * self.side_b
 
     def __add__(self, other):
-    
+
         other_perimeter = self.perimeter() + other.perimeter()
         other_side_a = self.side_a
         other_side_b = other_perimeter / 2 - other_side_a
@@ -23,9 +22,10 @@ class Rectangle:
         return Rectangle(other_side_a, other_side_b)
 
     def __sub__(self, other):
-   
+
         other_perimeter = abs(self.perimeter() - other.perimeter())
-        other_side_a = min([self.side_a, self.side_b, other.side_a, other.side_b])
+        other_side_a = min(
+            [self.side_a, self.side_b, other.side_a, other.side_b])
         other_side_b = other_perimeter / 2 - other_side_a
 
         return Rectangle(other_side_a, other_side_b)
