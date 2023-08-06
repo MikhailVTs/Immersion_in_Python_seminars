e5e1b6c3586792f48092af644d8a932fe3fa84ee
Tsaplin Mikhail <caplin.m@inbox.ru>
2023-08-06 15:19:16 +1000

    Решение алгоритма Марицы

--- Seminar_11/Matrix.py
+++ Seminar_11/Matrix.py
@@ -4,8 +4,7 @@ import numpy as np
 
 class Matrix:
 
-
-# операция для инициализации объектов
+    # операция для инициализации объектов
     def __init__(self, matrix: list[list]):
 
         self.value = matrix
@@ -14,24 +13,25 @@ class Matrix:
 
         self.height = len(matrix[0])
 
-
     def value(self):
 
         return self._value
-
 
     def value(self, matrix: list[list]):
         self._value = matrix
 
 
 # операция определения строкового представления объекта
+
+
     def __str__(self):
 
         return "\n".join(str(i) for i in self.value)
-    
 
 
 # операция определения поведения оператора равенства (==)
+
+
     def __eq__(self, other):
 
         if isinstance(other, Matrix):
@@ -47,11 +47,13 @@ class Matrix:
                         result.append(self.value[i][j] == other.value[i][j])
 
                 return all(result)
-            
+
         return False
-    
+
 
 # операции сравнения
+
+
     def __lt__(self, other):
 
         if isinstance(other, Matrix):
@@ -67,11 +69,13 @@ class Matrix:
                         result.append(self.value[i][j] < other.value[i][j])
 
                 return all(result)
-            
+
         return False
 
 
 # операции сложения
+
+
     def __add__(self, other):
 
         if isinstance(other, Matrix):
@@ -89,7 +93,9 @@ class Matrix:
                 return Matrix(result)
 
 
-#  операции умножения; 
+#  операции умножения;
+
+
     def __mul__(self, other):
 
         if isinstance(other, Matrix):
@@ -97,29 +103,29 @@ class Matrix:
             if self.height == other.length:
 
                 result = [[sum(a * b for a, b in zip(self_row, other_col))
-                           
+
                            for other_col in zip(*other.value)]
 
                           for self_row in self.value]
-                
+
             elif self.length == other.height:
 
                 result = [[sum(a * b for a, b in zip(self_col, other_row))
-                           
+
                            for self_col in zip(*self.value)]
 
                           for other_row in other.value]
-                
+
             return Matrix(result)
-        
+
         return False
 
 
 if __name__ == '__main__':
 
-    matrix_1 = Matrix(np.random.randint (0, 20,(3, 3)))
-    matrix_2 = Matrix(np.random.randint (0, 20,(3, 3)))
-    matrix_3 = Matrix(np.random.randint (0, 20,(3, 3)))
+    matrix_1 = Matrix(np.random.randint(0, 20, (3, 3)))
+    matrix_2 = Matrix(np.random.randint(0, 20, (3, 3)))
+    matrix_3 = Matrix(np.random.randint(0, 20, (3, 3)))
 
     print(f"\nМатрица №1\n{matrix_1}\n")
     print(f"\nМатрица №2\n{matrix_2}\n")
