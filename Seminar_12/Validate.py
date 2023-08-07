4ae683eff1dda36c8653dec093134f67234eef4f
Tsaplin Mikhail <caplin.m@inbox.ru>
2023-08-08 01:05:48 +1000

    Решение алгоритма валидности

--- Seminar_12/Validate.py
+++ Seminar_12/Validate.py
@@ -1,7 +1,7 @@
 class Validate:
 
     def __set_name__(self, owner, name):
-        
+
         self.param_name = '_' + name
 
     def __get__(self, instance, owner):
@@ -16,15 +16,19 @@ class Validate:
 
     def __delete__(self, instance):
 
-        raise AttributeError(f'Внимание эти данные не должны быть удалены - {self.param_name}')
+        raise AttributeError(
+            f'Внимание эти данные не должны быть удалены - {self.param_name}')
 
     def validate(self, value):
 
         if not isinstance(value, str):
-            raise TypeError(f'Внимание эти данные должны иметь текстовый формат - {value}')
-        
+            raise TypeError(
+                f'Внимание эти данные должны иметь текстовый формат - {value}')
+
         if not value.isalpha():
-            raise TypeError(f'Внимание эти данные должны состоять только из буквенных символов - {value}')
-        
+            raise TypeError(
+                f'Внимание эти данные должны состоять только из буквенных символов - {value}')
+
         if not value.istitle():
-            raise TypeError(f'Внимание эти данные должны воводиться с заглавной букввы - {value}')
+            raise TypeError(
+                f'Внимание эти данные должны воводиться с заглавной букввы - {value}')
